dc1135e61d5f47cf701342a86c177360bf13a911
nestor-labiuk <nestorlabiuk@gmail.com>
2024-01-26 18:28:49 -0300

    agrega enlace al mensaje del puerto para poder abrir ventana desde consola

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const app = require('./src/app')
 require('dotenv').config()
 
-app.listen(process.env.PORT, () => console.log('server listening on port', process.env.PORT))
+app.listen(process.env.PORT, () => {
+    console.log('server listening on port', process.env.PORT, '=>', `http://localhost:${process.env.PORT}`)
+})
