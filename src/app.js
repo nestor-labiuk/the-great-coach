30f5c6bb7b7db5d7088e6ba0c0d84dedb95258ba
nestor-labiuk <nestorlabiuk@gmail.com>
2024-01-26 22:45:02 -0300

    conecta el back a la base de datos

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 const coachesRouter = require('./routes/coaches.routes')
 const playersRouter = require('./routes/players.routes')
 
@@ -9,5 +11,9 @@ app.use(express.json())
 app.use('/api/coaches', coachesRouter)
 
 app.use('/api/players', playersRouter)
+
+mongoose.connect(process.env.MONGO_CONNECTION)
+    .then(() => console.log('Connected'))
+    .catch(() => console.log('Not Connected'))
 
 module.exports = app
